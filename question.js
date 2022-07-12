2591b567d9b163c02ea38f01d0c3b317c42a8b6e
omaranBazna <omran4d@gmail.com>
2022-07-11 23:59:39 -0400

    add the defintion for pure function

--- question.js
+++ question.js
@@ -13,5 +13,7 @@ const saySomethingAndReact = (msg = 'you forgot to submit a message') => {
 // add the inputs here
 saySomethingAndReact(message)(surprisedReaction);
 const createSpecialMessage=(name){
-    return "Hello, "+name
+    return "Hello, " + name
 }
+///what really make the function pure that every variable contained inside the function scope
+///so we do not expect to change the result by any other change in the code other than inputing arguments
